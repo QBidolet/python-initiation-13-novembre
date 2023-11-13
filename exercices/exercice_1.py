2ae3aa8c094cf9ad4309bbd3e0852cc7d9972ba9
Quentin BIDOLET <quentin.bidolet@gmail.com>
2023-11-13 14:24:25 +0100

    - exercice_1.py

---
+++ exercices/exercice_1.py
@@ -0,0 +1,13 @@
+"""
+Exercice 1 : Manipulation de Variables et Expressions
+Créer un script qui demande à l'utilisateur son nom,
+son âge, et sa ville de résidence,
+puis affiche ces informations dans une phrase complète.
+Exemple :
+Bonjour, je m'appelle Quentin, j'ai 29 ans et je vis à Paris.
+"""
+
+nom = input("Quel est votre nom ?\n")
+age = input("Quel est votre âge ?\n")
+ville = input("Où habitez-vous ?\n")
+print(f"Bonjour, je m'appelle {nom}, j'ai {age} ans et je vis à {ville}.")
