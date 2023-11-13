a1093f7d008e3c2278f2f2b97a3ec20a2b53c46e
Quentin BIDOLET <quentin.bidolet@gmail.com>
2023-11-13 14:06:05 +0100

    - print et input

---
+++ cours/01-syntaxes-de-bases/03-input.py
@@ -0,0 +1,7 @@
+# print("Veuillez saisir un nombre.")
+# Attention, input retourne TOUJOURS une chaine de caractères (string).
+nombre = input("Veuillez saisir un nombre.\n")
+# type renvoit le type de l'objet passé en paramètre.
+# pour convertir un string en entier, int()
+nombre = int(nombre)
+print(nombre)
