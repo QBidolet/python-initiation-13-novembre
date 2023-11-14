0af3169c754b1535a1688e7c53c7825d24ae48a4
Quentin BIDOLET <quentin.bidolet@gmail.com>
2023-11-14 15:58:51 +0100

    - exercice long

--- exercices/exercice_liste_long.py
+++ exercices/exercice_liste_long.py
@@ -8,14 +8,21 @@ Bonus : ne pas pouvoir saisir deux fois le même nom
 et afficher le numéro du chat à saisir
 ( exemple : saisir chat numéro 1 etc ... )
 """
-# boucler
-    # demander utiliseur saisie chat
-    # est ce que nom est déjà dans liste ?
-    # ajouter dans liste
-    # voulez vous continuer ?
 
-# affichage
+chats = []
+reponse = ""
+mots_stop = ["exit", "stop", "quitter", "quit"]
+while reponse.lower() not in mots_stop:
+    reponse = input("Entrez un nom de chat.\n")
+    if reponse.lower() not in mots_stop and reponse.strip() != "":
+        nom_chat = reponse.strip().capitalize()
+        if reponse not in chats:
+            chats.append(nom_chat)
+        else:
+            print("Vous avez déjà saisi ce nom de chat.")
 
+for indice, chat in enumerate(chats):
+    print(f"Le chat n°{indice + 1 } s'appelle {chat}.")
 
 
 
